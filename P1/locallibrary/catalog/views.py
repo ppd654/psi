12f5b22666c201570482c88e7d26627b6a5d2e72
HUgo <hugo.martinezr@estudiante.uam.es>
2026-02-12 23:49:28 +0100

    Hasta ejercicio 7

--- P1/locallibrary/catalog/views.py
+++ P1/locallibrary/catalog/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Book, BookInstance, Author, Genre
 
 # Create your views here.
+
+def index(request):
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    num_authors = Author.objects.count()
+
+    num_visits = request.session.get('num_visits', 0)
+    num_visits += 1
+    request.session['num_visits'] = num_visits
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_visits': num_visits,
+    }
+
+    return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 2
+class BookDetailView(generic.DetailView):
+    model = Book
+
+
+
