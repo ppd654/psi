aecea389773baf07a83a01d0230298a2c8ec36b5
HUgo <hugo.martinezr@estudiante.uam.es>
2026-02-15 19:55:33 +0100

    cambio al database en settings.py

--- P1/locallibrary/locallibrary/settings.py
+++ P1/locallibrary/locallibrary/settings.py
@@ -91,10 +91,10 @@ WSGI_APPLICATION = 'locallibrary.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config{
+    'default': dj_database_url.config(
         default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
         conn_max_age=600
-    }
+    )
 }
 
 
